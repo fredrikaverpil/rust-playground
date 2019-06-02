58d8b969e3dced1e1d76d4fa3336d933c440ae5f
Fredrik Averpil <fredrik@averpil.com>
2019-06-02 21:02:00 +0200

    Rearrange

--- src/enums.rs
+++ src/enums.rs
@@ -1,27 +1,6 @@
 #![allow(dead_code)]
 #![allow(unused)]
 
-struct Point {
-    x: f64,
-    y: f64
-}
-
-struct Line {
-    start: Point,
-    end: Point
-}
-
-fn structures() {
-    let p1 = Point {x: 0.0, y: 0.0};
-    println!("point p1 is at ({}, {})", p1.x, p1.y);
-
-    let p2 = Point {x: 3.0, y: 10.0};
-    println!("point p2 is at ({}, {})", p2.x, p2.y);
-
-    let line = Line {start: p1, end: p2};
-}
-
-
 enum Color {
     Red,
     Green,
@@ -46,7 +25,7 @@ fn enumerations() {
     }
 }
 
+
 pub fn main() {
-    structures();
     enumerations();
 }
---
+++ src/structs.rs
@@ -0,0 +1,26 @@
+#![allow(dead_code)]
+#![allow(unused)]
+
+struct Point {
+    x: f64,
+    y: f64
+}
+
+struct Line {
+    start: Point,
+    end: Point
+}
+
+fn structures() {
+    let p1 = Point {x: 0.0, y: 0.0};
+    println!("point p1 is at ({}, {})", p1.x, p1.y);
+
+    let p2 = Point {x: 3.0, y: 10.0};
+    println!("point p2 is at ({}, {})", p2.x, p2.y);
+
+    let line = Line {start: p1, end: p2};
+}
+
+pub fn main() {
+    structures();
+}
